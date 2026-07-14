9ad2f7c7a2b7e6a31707da58ac3baee283eac759
pawonGDigital6 <pawan@digitalsix.com.au>
2026-07-14 19:06:29 +0545

    style(woocommerce): add basic single product page styling

--- wp-content/themes/local-tasker/build/global/index.asset.php
+++ wp-content/themes/local-tasker/build/global/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-dom-ready'), 'version' => '4e915d0864a462c442ff');
+<?php return array('dependencies' => array('jquery', 'wp-dom-ready'), 'version' => '4085bbcce1a0bbedf5ff');
